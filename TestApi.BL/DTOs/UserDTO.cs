9923ad4d97ace2be3c89d768ed07bd6e61b24f5c
Your Name <kiievets1ivan@hneu.net>
2020-02-14 11:13:51 +0200

    updated proj2

---
+++ TestApi.BL/DTOs/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestApi.BL.DTOs
+{
+    public class UserDTO
+    {
+        [Required(ErrorMessage = "Fill in field 'First Name', please", AllowEmptyStrings = false)]
+        [StringLength(50, ErrorMessage = "First Name can't be over 50 symbols")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Fill in field 'Last Name', please", AllowEmptyStrings = false)]
+        [StringLength(50, ErrorMessage = "Last Name can't be over 50 symbols")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Fill in field 'Login', please", AllowEmptyStrings = false)]
+        [StringLength(255, ErrorMessage = "Login can't be over 255 symbols")]
+        [DataType(DataType.EmailAddress)]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Fill in field 'Password', please", AllowEmptyStrings = false)]
+        [StringLength(255, ErrorMessage = "Password can't be over 255 symbols and less 6 symbols", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ TestApi.BL/Services/UserService.cs
@@ -0,0 +1,121 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TestApi.BL.DTOs;
+using TestApi.BL.Models;
+using TestApi.BL.Services.Interfaces;
+using TestApi.DAL.Entities;
+using TestApi.DAL.Storages.Interfaces;
+
+namespace TestApi.BL.Services
+{
+    public class UserService : IUserService
+    {
+
+        private readonly IUserStorage _userStorage;
+        private readonly IMapper _mapper;
+        private readonly JwtSettings _jwtSettings;
+
+
+        public UserService(IUserStorage userStorage,
+                            IMapper mapper,
+                            JwtSettings jwtSettings)
+        {
+            _userStorage = userStorage;
+            _mapper = mapper;
+            _jwtSettings = jwtSettings;
+        }
+
+        public async Task<AuthResult> SignIn(UserAuth user)
+        {
+
+            var userForSignIn = await _userStorage.FindByName(user.Login);
+
+            if (userForSignIn != null)
+            {
+                if (await _userStorage.CheckPassword(userForSignIn, user.Password))
+                {
+                    return GenerateJwtToken(userForSignIn);
+                }
+
+                return new AuthResult
+                {
+                    Success = false,
+                    ErrorMessage = "Your password is invalid"
+                };
+            }
+
+            return new AuthResult
+            {
+                Success = false,
+                ErrorMessage = "User with this login dosen't exist"
+            };
+        }
+
+
+        public async Task<AuthResult> Register(UserDTO userDTO)
+        {
+            //var existingUser = _userManager.Users.Where(x => x.UserLogin == userDTO.Login);
+            var existingUser = await _userStorage.FindByName(userDTO.Login);
+
+            if (existingUser != null)
+            {
+                return new AuthResult
+                {
+                    Success = false,
+                    ErrorMessage = "User with this login already exists"
+                };
+            }
+
+            var newUser = _mapper.Map<UserEntity>(userDTO);
+
+            var createdUser = await _userStorage.Create(newUser, userDTO.Password);
+
+            if (!createdUser.Succeeded)
+            {
+                return new AuthResult
+                {
+                    Success = false,
+                    ErrorMessage = "Smth went wrong"
+                };
+            }
+
+            return new AuthResult
+            {
+                Success = true,
+            };
+        }
+
+        private AuthResult GenerateJwtToken(UserEntity user)
+        {
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserLogin),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim("id", user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(30),//lifetime
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescription);
+
+            return new AuthResult
+            {
+                Success = true,
+                Token = tokenHandler.WriteToken(token)
+            };
+        }
+    }
+}
