13593fa946d1513b212b3df7a37162fa7f752a12
Your Name <kiievets1ivan@hneu.net>
2020-02-14 11:13:01 +0200

    rm useless folders

--- TestApi.DAL/Entities/AuthResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestApi.DAL.Entities
-{
-    public class AuthResult
-    {
-        public bool Success { get; set; }
-        public string Token { get; set; }
-        public string ErrorMessage { get; set; }
-    }
-}
